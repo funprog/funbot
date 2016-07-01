f50da72ec6a8526858addb766ff800adfbdb18ca
Jin-Wook Chung <jwChung@hotmail.com>
2016-07-01 16:19:04 +0900

    Addressed rectifying a test.

--- src/test/scala/DiceBotTest.scala
+++ src/test/scala/DiceBotTest.scala
@@ -30,13 +30,12 @@ class DiceBotTest extends FunSuite {
 
   Seq(
     "invalid string",
-    null,
     "").foreach(x => {
     test(s"process with invalid input '$x' returns none.") {
       val expected = None
       val sut = new DiceBot()
 
-      val actual = sut.process("invalid string")
+      val actual = sut.process(x)
 
       assert(expected == actual)
     }
