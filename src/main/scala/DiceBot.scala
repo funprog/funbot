28ca172251877adaaf1c5815d8125e6a6203e0b3
Jin-Wook Chung <jwChung@hotmail.com>
2016-07-01 13:29:35 +0900

    Add the document on the process method.

--- src/main/scala/DiceBot.scala
+++ src/main/scala/DiceBot.scala
@@ -1,3 +1,13 @@
 object DiceBot {
+  /** Processes an `input` to return sequential string values
+    * representing order.
+    *
+    * @param input consists of two numbers concatenated using the 'd' character.
+    *              (eg. "2d4") The two numbers represent the number of
+    *              rolling and the number of dice dimensions(inclusive)
+    *              respectively.
+    * @return returns `None` if the `input` is invalid, otherwise `Some[String]`
+    *         representing the order.
+    */
   def process(input: String): Option[String] = None
 }
