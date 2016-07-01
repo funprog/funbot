638c6d3b9b51d25110a2ac6eb72d78dfe5f9594e
Jin-Wook Chung <jwChung@hotmail.com>
2016-07-01 16:16:46 +0900

    Added guard clauses and the random property.

--- src/main/scala/DiceBot.scala
+++ src/main/scala/DiceBot.scala
@@ -1,4 +1,10 @@
-class DiceBot {
+import scala.util.Random
+
+class DiceBot(val random: Random) {
+  require(random != null, "the 'random' value cannot be null.")
+
+  def this() = this(new Random())
+
   private val regex = """(\d)d(\d)""".r
 
   /** Processes an `input` to return sequential string values
@@ -11,9 +17,12 @@ class DiceBot {
     * @return returns `None` if the `input` is invalid, otherwise `Some[String]`
     *         representing the order.
     */
-  def process(input: String): Option[String] =
+  def process(input: String): Option[String] = {
+    require(input != null, "the 'input' value cannot be null.")
+
     input match {
       case regex(x, y) => Some("1")
       case _ => None
     }
+  }
 }
--- src/test/scala/DiceBotTest.scala
+++ src/test/scala/DiceBotTest.scala
@@ -1,6 +1,33 @@
 import org.scalatest.FunSuite
+import scala.util.Random
 
 class DiceBotTest extends FunSuite {
+  test("constructor with null random throws") {
+    val thrown = intercept[IllegalArgumentException] (new DiceBot(null))
+    assert(thrown != null)
+  }
+
+  test("random is correct when initialized with default constructor.") {
+    val sut = new DiceBot()
+    val actual = sut.random
+    assert(classOf[Random] == actual.getClass)
+  }
+
+  test("random is correct when initialized with a random.") {
+    val expected = new Random()
+    val sut = new DiceBot(expected)
+
+    val actual = sut.random
+
+    assert(expected == actual)
+  }
+
+  test("process with null input throws") {
+    val thrown = intercept[IllegalArgumentException] (
+      new DiceBot().process(null))
+    assert(thrown != null)
+  }
+
   Seq(
     "invalid string",
     null,
