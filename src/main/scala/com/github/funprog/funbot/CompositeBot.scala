e0ada3a353864e677e28fd412895cf7dbec6b3b6
Jin-Wook Chung <jwChung@hotmail.com>
2016-07-12 16:38:48 +0900

    Implement CompositeBot class

---
+++ src/main/scala/com/github/funprog/funbot/CompositeBot.scala
@@ -0,0 +1,10 @@
+package com.github.funprog.funbot
+
+class CompositeBot(val bots: Bot*) extends Bot {
+  override def process(input: String): Option[String] =
+    bots.toStream.map(x => x.process(input))
+        .find(x => x != None) match {
+      case Some(result) => result
+      case None => None
+    }
+}
---
+++ src/test/scala/com/github/funprog/funbot/CompositeBotTest.scala
@@ -0,0 +1,49 @@
+package com.github.funprog.funbot
+
+import org.scalatest.FunSuite
+
+class CompositeBotTest extends FunSuite {
+  test("sut is Bot.") {
+    val sut = new CompositeBot
+    assert(sut.isInstanceOf[Bot])
+  }
+
+  test("process returns None when all bots return None.") {
+    val bot1 = new DelegatingBot
+    val bot2 = new DelegatingBot
+    val bot3 = new DelegatingBot
+    bot1.onProcess = _ => None
+    bot2.onProcess = _ => None
+    bot3.onProcess = _ => None
+    val sut = new CompositeBot(bot1, bot2, bot3)
+
+    val actual = sut.process("dummy")
+
+    assert(None == actual)
+  }
+
+  test("process returns first result from bots.") {
+    val input = "input"
+    val expected = Some("result")
+    val bot1 = new DelegatingBot
+    val bot2 = new DelegatingBot
+    val bot3 = new DelegatingBot
+    bot1.onProcess = _ => None
+    bot2.onProcess = x => {
+      assert(x == input)
+      expected
+    }
+    bot3.onProcess = _ => throw new Exception
+    val sut = new CompositeBot(bot1, bot2, bot3)
+
+    val actual = sut.process(input)
+
+    assert(expected == actual)
+  }
+
+  class DelegatingBot extends Bot{
+    var onProcess: String => Option[String] = null
+
+    override def process(input: String): Option[String] = onProcess(input)
+  }
+}
